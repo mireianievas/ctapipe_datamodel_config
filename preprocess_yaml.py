bfefea00eb43ada1d75b780d0b275bf707d7351f
Mireia Nievas <mnievas@ucm.es>
2016-10-24 17:39:34 +0200

    Minor style corrections

--- preprocess_yaml.py
+++ preprocess_yaml.py
@@ -16,6 +16,29 @@ import numpy as np
 import sys
 from urllib.request import urlopen
 
+
+
+class File(str):
+    #yaml_tag = u'!File'
+    def __init__(self, filename):
+        self.filename = filename
+    def read(self):
+        with open(self.filename,"r") as fin:
+            return(yaml.load(fin))
+    def append(self,data):
+        with open(self.filename,"a+") as fout:
+            yaml.dump(data, fout, Dumper=Dumper)
+    def write(self,data):
+        with open(self.filename,"r+") as fout:
+            yaml.dump(data, fout, Dumper=Dumper)
+    def truncate(self):
+        with open(self.filename,"w") as fout:
+            fout.write("")
+    def __new__(cls, a):
+        return str.__new__(cls, a)
+    def __repr__(self):
+        return "File(%s)" % self
+
 def extfile_representer(dumper, data):
     return dumper.represent_scalar(u'!file', data.filename)
 
@@ -23,29 +46,6 @@ def extfile_constructor(loader, node):
     from ruamel.yaml.nodes import ScalarNode
     if isinstance(node, ScalarNode):
         return File(node.value)
-
-class File(str):
-    #yaml_tag = u'!File'
-    def __init__(self, filename):
-        self.filename = filename
-
-    def read(self):
-        with open(self.filename,"r") as fin:
-            return(yaml.load(fin))
-    def append(self,data):
-        with open(self.filename,"a+") as fout:
-            yaml.dump(data, fout, Dumper=MyDumper)
-    def write(self,data):
-        with open(self.filename,"r+") as fout:
-            yaml.dump(data, fout, Dumper=MyDumper)
-    def truncate(self):
-        with open(self.filename,"w") as fout:
-            fout.write("")
-
-    def __new__(cls, a):
-        return str.__new__(cls, a)
-    def __repr__(self):
-        return "File(%s)" % self
 
 yaml.add_representer(File, extfile_representer)
 yaml.add_constructor(u'!file', extfile_constructor)
