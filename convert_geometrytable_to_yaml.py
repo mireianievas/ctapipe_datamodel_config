4d661b29e9ee8331bcf1ad0e5a3081fa83157f9f
Mireia Nievas <mnievas@ucm.es>
2016-10-24 17:33:52 +0200

    preprocess_yaml is now able, through recursive_build, to explore explore the yaml tree and load any additional file defined there with the !file "myfilename" tag.

--- convert_geometrytable_to_yaml.py
+++ convert_geometrytable_to_yaml.py
@@ -5,8 +5,10 @@ from astropy.io import ascii
 from astropy.units import Quantity
 import ruamel.yaml as yaml
 from collections import OrderedDict
-from ruamel.yaml.constructor import SafeConstructor
-from ruamel.yaml.representer import SafeRepresenter, RoundTripRepresenter
+from ruamel.yaml.loader      import Loader
+from ruamel.yaml.dumper      import Dumper
+from ruamel.yaml.constructor import Constructor
+from ruamel.yaml.representer import Representer
 import ruamel.yaml.comments as Comments
 import numpy as np
 import sys
@@ -17,14 +19,57 @@ templatef_remote = "https://raw.githubusercontent.com/mireianievas/ctapipe_datam
 infile_camera  = "https://drive.google.com/uc?export=download&id=0B4OIF0_Zm04WbFdfTzBuOTQ2em8"
 infile_mirrors = "https://raw.githubusercontent.com/mireianievas/ctapipe_datamodel_config/master/configOpticsGCT.yaml"
 
-
-
 ##### YAML Handlers
 def quantity_representer(dumper, data):
     item_key   = float(data.value)
     item_value = str(data.unit)
     value = "%s %s" %(item_key,item_value)
     return yaml.representer_str(value)
+def dict_representer(dumper, data):
+    return dumper.represent_dict(data.items())
+def dict_constructor(loader, node):
+    return OrderedDict(loader.construct_pairs(node))
+def construct_yaml_odict(loader, node):
+    from itertools import chain
+    from ruamel.yaml.compat import ordereddict
+    merge_map = loader.flatten_mapping(node)
+    omap_gen=loader.construct_yaml_omap(node)
+    omap = ordereddict((k,v) for (k,v) in omap_gen)
+    if merge_map:
+        omap.add_yaml_merge(merge_map)
+    yield omap
+
+
+class Loader_map_as_anydict( object):
+    'inherit + Loader'
+    anydict = None      #override
+    @classmethod        #and call this
+    def load_map_as_anydict( klas):
+        yaml.add_constructor( 'tag:yaml.org,2002:map', klas.construct_yaml_map)
+
+    'copied from constructor.BaseConstructor, replacing {} with self.anydict()'
+    def construct_mapping(self, node, deep=False):
+        if not isinstance(node, MappingNode):
+            raise ConstructorError(None, None,
+                    "expected a mapping node, but found %s" % node.id,
+                    node.start_mark)
+        mapping = self.anydict()
+        for key_node, value_node in node.value:
+            key = self.construct_object(key_node, deep=deep)
+            try:
+                hash(key)
+            except TypeError as exc:
+                raise ConstructorError("while constructing a mapping", node.start_mark,
+                        "found unacceptable key (%s)" % exc, key_node.start_mark)
+            value = self.construct_object(value_node, deep=deep)
+            mapping[key] = value
+        return mapping
+
+    def construct_yaml_map( self, node):
+        data = self.anydict()
+        yield data
+        value = self.construct_mapping(node)
+        data.update(value)
 
 if np is not None:
     # Represent 1d ndarrays as lists in yaml files because it makes them much
@@ -36,60 +81,37 @@ if np is not None:
             return dumper.represent_list([[l for l in k] for k in data])
         else:
             return dumper.represent_list(data.tolist())
-    yaml.add_representer(np.ndarray, ndarray_representer)
-
-    # represent numpy types as things that will print more cleanly
     def complex_representer(dumper, data):
         return dumper.represent_scalar('!complex', repr(data.tolist()))
-    yaml.add_representer(np.complex128, complex_representer)
-    yaml.add_representer(np.complex, complex_representer)
     def complex_constructor(loader, node):
         return complex(node.value)
-    yaml.add_constructor('!complex', complex_constructor)
-
     def numpy_float_representer(dumper, data):
         return dumper.represent_float(float(data))
-    yaml.add_representer(np.float64, numpy_float_representer)
-
     def numpy_int_representer(dumper, data):
         return dumper.represent_int(int(data))
-    yaml.add_representer(np.int64, numpy_int_representer)
-    yaml.add_representer(np.int32, numpy_int_representer)
-
     def numpy_dtype_representer(dumper, data):
         return dumper.represent_scalar('!dtype', data.name)
-    yaml.add_representer(np.dtype, numpy_dtype_representer)
-
     def numpy_dtype_loader(loader, node):
         name = loader.construct_scalar(node)
         return np.dtype(name)
-    yaml.add_constructor('!dtype', numpy_dtype_loader)
 
+    Representer.add_representer(np.ndarray, ndarray_representer)
+    Representer.add_representer(np.complex128, complex_representer)
+    Representer.add_representer(np.complex, complex_representer)
+    Representer.add_representer(np.float64, numpy_float_representer)
+    Representer.add_representer(np.int64, numpy_int_representer)
+    Representer.add_representer(np.int32, numpy_int_representer)
+    Representer.add_representer(np.dtype, numpy_dtype_representer)
+    Constructor.add_constructor('!complex', complex_constructor)
+    Constructor.add_constructor('!dtype', numpy_dtype_loader)
 
-yaml.add_representer(Quantity, quantity_representer)
-
-yaml.add_representer(Comments.CommentedSeq,
-  RoundTripRepresenter.represent_list)
-
-yaml.add_representer(Comments.CommentedMap,
-  RoundTripRepresenter.represent_dict)
-
-yaml.add_representer(Comments.CommentedOrderedMap,
-  RoundTripRepresenter.represent_ordereddict)
-
-yaml.add_representer(Comments.CommentedSet, \
-  RoundTripRepresenter.represent_set)
-
-def dict_representer(dumper, data):
-    return dumper.represent_dict(data.items())
-
-def dict_constructor(loader, node):
-    return OrderedDict(loader.construct_pairs(node))
-
-yaml.add_representer(OrderedDict, dict_representer)
+Representer.add_representer(Quantity, quantity_representer)
+Representer.add_representer(OrderedDict, dict_representer)
+#Constructor.add_constructor(u'tag:yaml.org,2002:map',construct_yaml_odict)
 
 # remove the idXXX references in the dumper
-class MyDumper(yaml.Dumper):
+MyLoader = Loader
+class MyDumper(Dumper):
     def ignore_aliases(self, _data):
         return True
 
@@ -113,10 +135,10 @@ def unique(items):
 
 # Read the content and the template
 with urlopen(templatef_remote) as fin:
-    YamlTemplate = yaml.load(fin)
+    YamlTemplate = yaml.load(fin, Loader=MyLoader)
 raw_camgeometry_file_content = ascii.read(infile_camera)
 with urlopen(infile_mirrors) as fin:
-    YamlOptics = yaml.load(fin)
+    YamlOptics = yaml.load(fin, Loader=MyLoader)
 
 ### Create Drawers
 Telescope   = OrderedDict(YamlTemplate['Telescope'])
@@ -167,13 +189,44 @@ Optics = YamlOptics['TelescopeOpticsGCT']
 with open(optoutfile,"w+") as fout:
     yaml.round_trip_dump(Optics, fout, Dumper=MyDumper)
 
+def extfile_representer(dumper, data):
+    return dumper.represent_scalar(u'!file', data.filename)
+
+def extfile_constructor(loader, node):
+    return File(node)
+
+class File(str):
+    #yaml_tag = u'!File'
+    def __init__(self, filename):
+        self.filename = filename
+
+    def read(self):
+        with open(self.filename,"r") as fin:
+            return(fin)
+    def append(self,data):
+        with open(self.filename,"a+") as fout:
+            fout.write(data)
+    def write(self,data):
+        with open(self.filename,"r+") as fout:
+            fout.write(data)
+    def truncate(self):
+        with open(self.filename,"w") as fout:
+            fout.write("")
+
+    def __new__(cls, a):
+        return str.__new__(cls, a)
+    def __repr__(self):
+        return "File(%s)" % self
+
+yaml.add_representer(File, extfile_representer)
+yaml.add_constructor(u'!file', extfile_constructor)
+
 def yaml_include_file(filename):
-    content = {}
-    content['file'] = filename
-    return(content)
+    return File(filename)
 
 YamlObject = OrderedDict()
 YamlObject['GCT_Telescope_Prototype'] = Telescope
+Telescope["ID"]='GCT_Telescope_Prototype'
 Telescope["camera"] = yaml_include_file(camoutfile)
 Telescope["optics"] = yaml_include_file(optoutfile)
 
--- preprocess_yaml.py
+++ preprocess_yaml.py
@@ -1,1 +1,72 @@
 #!/bin/env python3
+
+#!/bin/env python3
+
+from astropy.table import Table
+from astropy.io import ascii
+from astropy.units import Quantity
+import ruamel.yaml as yaml
+from collections import OrderedDict
+from ruamel.yaml.loader      import Loader
+from ruamel.yaml.dumper      import Dumper
+from ruamel.yaml.constructor import Constructor
+from ruamel.yaml.representer import Representer
+import ruamel.yaml.comments as Comments
+import numpy as np
+import sys
+from urllib.request import urlopen
+
+def extfile_representer(dumper, data):
+    return dumper.represent_scalar(u'!file', data.filename)
+
+def extfile_constructor(loader, node):
+    from ruamel.yaml.nodes import ScalarNode
+    if isinstance(node, ScalarNode):
+        return File(node.value)
+
+class File(str):
+    #yaml_tag = u'!File'
+    def __init__(self, filename):
+        self.filename = filename
+
+    def read(self):
+        with open(self.filename,"r") as fin:
+            return(yaml.load(fin))
+    def append(self,data):
+        with open(self.filename,"a+") as fout:
+            yaml.dump(data, fout, Dumper=MyDumper)
+    def write(self,data):
+        with open(self.filename,"r+") as fout:
+            yaml.dump(data, fout, Dumper=MyDumper)
+    def truncate(self):
+        with open(self.filename,"w") as fout:
+            fout.write("")
+
+    def __new__(cls, a):
+        return str.__new__(cls, a)
+    def __repr__(self):
+        return "File(%s)" % self
+
+yaml.add_representer(File, extfile_representer)
+yaml.add_constructor(u'!file', extfile_constructor)
+
+### expand
+
+with open("gct_telescope_output_test_v00.yaml") as fin:
+    Telescope = yaml.load(fin)['GCT_Telescope_Prototype']
+
+def recursive_build(item):
+    for subitem in item:
+        content = item[subitem]
+        if type(content) is File:
+            with open(content,'r') as fin:
+                item[subitem] = content.read()
+                item[subitem]['fromfilename'] = content.filename
+                content = item[subitem]
+
+        if type(content) is dict:
+            content = recursive_build(content)
+
+        item[subitem] = content
+
+    return item
